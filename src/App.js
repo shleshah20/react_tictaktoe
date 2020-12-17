0ca17bfa4f60bba1dea137dd2c110a81420c7be8
shlesha shah <shleshah20@gmail.com>
2020-12-18 00:27:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Game from './componets/game';
+
+function App() {
+  return (
+    <div className="container">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
